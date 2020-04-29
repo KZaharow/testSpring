97fea5679d29d760ce0d18320375d9d6863e7925
kzaharow <kzaharow@gmail.com>
2020-04-29 15:57:51 +0300

    Initial commit

---
+++ src/main/java/com/zaharov/testSpring/controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.zaharov.testSpring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String getMainPage(Model model) {
+        model.addAttribute("myText", "super puper text");
+        return "mainPage";
+    }
+}
